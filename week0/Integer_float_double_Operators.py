795f7cefe81ee97f7e8428eaaae48b305d9d3adf
ACER <ACER@LAPTOP-T12N7CIU>
2026-07-31 16:06:04 +0530

    MR#4 functions, [ ints, floats, operators ], string methods

---
+++ week0/Integer_float_double_Operators.py
@@ -0,0 +1,17 @@
+x = int(input("whats x? :"))
+y = int(input("whats y? :"))
+
+print(x+y)
+
+
+
+x = float(input("whats x? :"))
+y = float(input("whats y? :"))
+
+print(x+y)
+
+x = float(input("whats x? :"))
+y = float(input("whats y? :"))
+
+print(f"{round(x+y):,}")
+
